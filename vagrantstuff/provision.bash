469165b721b8ef0f0dc6990174b0dcc45026dd1b
Michael McEniry [UAH] <mcenirm@uah.edu>
2017-02-12 12:38:54 -0600

    Avoid running yum if package is already installed

--- vagrantstuff/provision.bash
+++ vagrantstuff/provision.bash
@@ -3,9 +3,26 @@
 set -e
 set -u
 
-sudo yum -y install lsof strace
-sudo yum -y install epel-release
-sudo yum -y install jq
+
+check_and_install_package () {
+  local name
+  local missing=()
+
+  for name in "$@" ; do
+    if ! rpm --quiet -q "$name" ; then
+      missing+=( "$name" )
+    fi
+  done
+
+  if [ "${#missing[@]}" -gt 0 ] ; then
+    sudo yum -y install "${missing[@]}"
+  fi
+}
+
+
+check_and_install_package lsof strace
+check_and_install_package epel-release
+check_and_install_package jq
 
 mkdir -p ~/try1
 cp -v -p /vagrant/try1/metricbeat.yml ~/try1/
