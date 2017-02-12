4003f80f88b14651f23c6a6f827b8ae0d11c9d67
Michael McEniry [UAH] <mcenirm@uah.edu>
2017-02-12 12:47:58 -0600

    Be able to install package by owned file

--- vagrantstuff/provision.bash
+++ vagrantstuff/provision.bash
@@ -4,12 +4,21 @@ set -e
 set -u
 
 
+looks_like_a_file_and_not_a_package_name () {
+  ! [ "$1" = "${name##/}" ]
+}
+
+
 check_and_install_package () {
   local name
   local missing=()
 
   for name in "$@" ; do
-    if ! rpm --quiet -q "$name" ; then
+    check_cmd=( rpm --quiet -q )
+    if looks_like_a_file_and_not_a_package_name "$name" ; then
+      check_cmd+=( --file )
+    fi
+    if ! "${check_cmd[@]}" "$name" ; then
       missing+=( "$name" )
     fi
   done
@@ -23,6 +32,8 @@ check_and_install_package () {
 check_and_install_package lsof strace
 check_and_install_package epel-release
 check_and_install_package jq
+check_and_install_package /usr/bin/xxd
+
 
 mkdir -p ~/try1
 cp -v -p /vagrant/try1/metricbeat.yml ~/try1/
